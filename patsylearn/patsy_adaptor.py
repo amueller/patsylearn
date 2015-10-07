169931836f56fcf26c7b30c75ccd5b31cdfe5a9e
Andreas Mueller <amueller@nyu.edu>
2015-10-06 17:07:48 -0700

    make PatsyModel functional

--- patsylearn/patsy_adaptor.py
+++ patsylearn/patsy_adaptor.py
@@ -1,10 +1,11 @@
 import numpy as np
 
 from sklearn.base import BaseEstimator, TransformerMixin, clone
+from sklearn.utils.metaestimators import if_delegate_has_method
 from patsy import dmatrix, dmatrices
 
 
-### TODO: take care of intercepts
+# TODO: take care of intercepts
 
 class PatsyModel(BaseEstimator):
     def __init__(self, estimator, formula):
@@ -23,28 +24,33 @@ class PatsyModel(BaseEstimator):
         design_y, design_X = dmatrices(self.formula, data)
         self.design_y_ = design_y.design_info
         self.design_X_ = design_X.design_info
-        X =  np.array(design_X)
-        y =  np.array(design_y)
         est = clone(self.estimator)
-        self.estimator_ = est.fit(X, y)
+        self.estimator_ = est.fit(design_X, design_y)
         return self
 
+    @if_delegate_has_method(delegate='estimator')
     def predict(self, data):
         X = np.array(dmatrix(self.design_X_, data))
-        return self.estimator.predict(X)
+        return self.estimator_.predict(X)
 
-    def transform(self, X):
-        pass
+    @if_delegate_has_method(delegate='estimator')
+    def transform(self, data):
+        X = np.array(dmatrix(self.design_X_, data))
+        return self.estimator_.transform(X)
 
+    @if_delegate_has_method(delegate='estimator')
     def score(self, data):
-        pass
+        design_infos = (self.design_y_, self.design_X_)
+        design_y, design_X = dmatrices(design_infos, data)
+        return self.estimator_.score(design_X, design_y)
+
 
 class PatsyTransformer(BaseEstimator, TransformerMixin):
     def __init__(self, formula):
         self.formula = formula
 
     def fit(self, data):
-        pass
+        return self
 
     def transform(self, data):
         pass
