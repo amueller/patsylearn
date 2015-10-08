b43964af5c84a7b3f0c39460726f9080187aa946
Andreas Mueller <amueller@nyu.edu>
2015-10-07 19:12:39 -0700

    add minimal setup.py, version

--- patsylearn/__init__.py
+++ patsylearn/__init__.py
@@ -1,3 +1,4 @@
 from .patsy_adaptor import PatsyModel, PatsyTransformer
 
 __all__ = ['PatsyModel', 'PatsyTransformer']
+__version__ = "0.0.1"
---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import patsylearn
+
+from os.path import exists
+from setuptools import setup
+
+setup(name='dask',
+      version=patsylearn.__version__,
+      description='Scikit-lean Patsy adaptor',
+      url='http://github.com/amueller/patsylearn/',
+      maintainer='Andreas Mueller',
+      maintainer_email='t3kcit@gmail.com',
+      license='BSD',
+      keywords='scikit-learn patsy formula machine-learning',
+      packages=['patsylearn'],
+      long_description=(open('README.md').read() if exists('README.rst') else
+                        ''))
